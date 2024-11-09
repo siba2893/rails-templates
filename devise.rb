0b35c2f314138e32b613a40b9949cdf5df9eb612
Daniel Sibaja <siba2893@gmail.com>
2024-11-09 09:40:06 -0600

    Add .env.development

--- devise.rb
+++ devise.rb
@@ -109,6 +109,10 @@ general_config = <<~RUBY
   config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
 RUBY
 
+# ENV
+########################################
+run "touch .env.dev"
+
 environment general_config
 
 ########################################
